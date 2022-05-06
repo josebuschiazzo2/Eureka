a173c2f1c45c91005a7bb1657bdee9516dd6c913
josebuschiazzo2 <104038315+josebuschiazzo2@users.noreply.github.com>
2022-05-06 14:19:52 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-import "./styles.css";
+/*Eureca*/
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+let count:number=0;
+while(count<3){
+  let password: string = prompt("Ingrese su contraseña");
+  if (password!== "eureka") {
+    count++;
+    alert("Contraseña incorrecta. Tiene " + (3-count) + " intentos restantes");
+  } else {
+      count=3;
+      alert ("Contraseña Correcta. Saliendo del programa");
+    }
+}
